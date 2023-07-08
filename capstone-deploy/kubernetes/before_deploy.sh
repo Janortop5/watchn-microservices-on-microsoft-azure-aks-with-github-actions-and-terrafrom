9c0e429d9cf4c291a25f720c25e119d2b6226a67
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-07-08 10:55:13 +0000

    Add new file

---
+++ capstone-deploy/kubernetes/before_deploy.sh
@@ -0,0 +1,41 @@
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get install -y gnupg software-properties-common wget curl gpg
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    tee /etc/apt/sources.list.d/hashicorp.list
+
+apt update
+
+apt-get install terraform
+
+apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+./aws/install
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+
+apt-get install apt-transport-https --yes
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+apt-get update
+
+apt-get install helm
+
+helm repo add stable https://charts.helm.sh/stable
+
+helm repo update
