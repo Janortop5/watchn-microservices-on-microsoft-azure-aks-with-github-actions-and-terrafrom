895655ca3377ef5401a567eadc5ea054cb563052
Aladejana, Emmanuel Abiodun <dejanaemzy37@gmail.com>
2023-07-08 11:11:57 +0000

    Update before_deploy.sh

--- capstone-deploy/before_deploy.sh
+++ capstone-deploy/before_deploy.sh
@@ -22,21 +22,7 @@ helm repo add stable https://charts.helm.sh/stable
 
 helm repo update
 
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    tee /etc/apt/sources.list.d/hashicorp.list
-
-apt update
-
-apt-get install terraform
+snap install terraform --classic
 
 helm
 
